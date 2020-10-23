9e56888b777e7fb5e9f64eea8db8036b66155b00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-23 06:36:00 +0000

    Deploy laura-chou/vue_photo to github.com/laura-chou/vue_photo.git:gh-pages

---
+++ precache-manifest.aea65b5b69effbc9027cb0e3f9fb6f70.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "422429f36dea5bb8b3e1",
+    "url": "css/app.362408c8.css"
+  },
+  {
+    "revision": "23e51dcc2e9cd8cbaffd",
+    "url": "css/chunk-vendors.09cf8b16.css"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "6e604b4368eee593fd8173c955e2f27f",
+    "url": "img/add-user.png"
+  },
+  {
+    "revision": "e398180365f20cb2a5a8e197e238c408",
+    "url": "img/add_file.png"
+  },
+  {
+    "revision": "9837f128f8381a14234b2dd810fab65f",
+    "url": "img/add_image.png"
+  },
+  {
+    "revision": "9306ae5378256223715ca658af3d85ef",
+    "url": "img/bg.png"
+  },
+  {
+    "revision": "fd9501babe8df929a79c1a5ef37acdba",
+    "url": "img/bg1.png"
+  },
+  {
+    "revision": "2968abab712949b7faa33bcfe1b87e76",
+    "url": "img/empty_file.png"
+  },
+  {
+    "revision": "5fde43b00a8f04652aa8a51979736c50",
+    "url": "img/file.png"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "2108b72f39a26171f71a7d8be0321c46",
+    "url": "img/garbage.png"
+  },
+  {
+    "revision": "fba2c2ae890ce7541036ad4923042da7",
+    "url": "img/logo.png"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "acdcdf6f3b424bc5c3815998dc4d1acc",
+    "url": "img/photo-album.png"
+  },
+  {
+    "revision": "fbc9a6bc41b89d6b31596d4710a7de5c",
+    "url": "img/user.png"
+  },
+  {
+    "revision": "faf99a6b61dffa0531d1ff0dd43b5ed3",
+    "url": "index.html"
+  },
+  {
+    "revision": "47c895af184be8059e66",
+    "url": "js/album.7d96cd99.js"
+  },
+  {
+    "revision": "422429f36dea5bb8b3e1",
+    "url": "js/app.c5dd7afd.js"
+  },
+  {
+    "revision": "23e51dcc2e9cd8cbaffd",
+    "url": "js/chunk-vendors.9d10a105.js"
+  },
+  {
+    "revision": "c40015c4bf33cda774aa",
+    "url": "js/login.7911c027.js"
+  },
+  {
+    "revision": "971a6e2987ac8a5edafc",
+    "url": "js/photo.9ef25290.js"
+  },
+  {
+    "revision": "70356c7688092691944b",
+    "url": "js/register.7f9abdcb.js"
+  },
+  {
+    "revision": "6838a3744c941569fd0d002d13902eb4",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
